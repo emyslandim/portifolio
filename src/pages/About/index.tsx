11fc2d5196778f00624062d49746e18ccc66079d
emyslandim <emydl.moura@gmail.com>
2023-02-06 20:51:33 -0300

    Improve about page

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -1,5 +1,44 @@
 import { Ui } from "./styles";
 
 export default function About() {
-  return <span>About</span>;
+  return (
+    <Ui.Wrapper>
+      <Ui.Title>
+        <span>01.</span> About me
+      </Ui.Title>
+      <Ui.Description>
+        <section>
+          <p>
+            Hey! My name is Emilly and I enjoy creating things to the internet.
+            My interest in web development started when I was a 15 year old kid
+            who was determined to make your tumblr the cutest ever editing
+            custom Tumblr themes — turns out hacking together a custom reblog
+            button taught me a lot about HTML & CSS!
+          </p>
+          <p>
+            Fast-forward to today, and <Ui.Marked>I’ve had the privilege of working at an a
+            start-up, a huge corporation, and an ecommerce</Ui.Marked>. My main focus these
+            days is building accessible, inclusive products and digital
+            experiences at Upstatement for a variety of clients.
+          </p>
+          <p>Here are a few technologies I’ve been working with recently:</p>
+          <Ui.SkillList>
+            <li>JavaScript (ES6+)</li>
+            <li>TypeScript</li>
+            <li>React</li>
+            <li>Vue.js</li>
+            <li>Angular</li>
+            <li>Node.js</li>
+          </Ui.SkillList>
+        </section>
+        <Ui.ProfilePic
+          src="/images/profile.jpeg"
+          alt="Profile pic"
+          width={300}
+          height={300}
+          priority
+        />
+      </Ui.Description>
+    </Ui.Wrapper>
+  );
 }
--- src/pages/About/styles.ts
+++ src/pages/About/styles.ts
@@ -1,17 +1,76 @@
 import styled from "styled-components";
+import Image from "next/image";
 
-const Button = styled.a`
-  background-color: transparent;
-  border: 1px solid var(--green);
-  color: var(--green);
-  background-color: transparent;
-  border-radius: 4px;
-  padding: 0.75rem 1rem;
-  font-size: 13px;
-  line-height: 1;
-  text-decoration: none;
-  cursor: pointer;
-  transition: var(--transition);
+const Wrapper = styled.main`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  margin: 0px 24%;
 `;
 
-export const Ui = { Button };
+const Description = styled.div`
+  display: grid;
+  grid-template-columns: 3fr 2fr;
+  gap: 50px;
+  color: var(--slate);
+  font-size: 16px;
+
+  p {
+    display: block;
+    margin-block-start: 1em;
+    margin-block-end: 1em;
+    margin-inline-start: 0px;
+    margin-inline-end: 0px;
+
+    margin: 0px 0px 15px;
+  }
+`;
+
+const Title = styled.h1`
+  display: flex;
+  -webkit-box-align: center;
+  align-items: center;
+  position: relative;
+  margin: 10px 0px 40px;
+  width: 100%;
+  font-size: clamp(26px, 5vw, var(--fz-heading));
+  white-space: nowrap;
+
+  span {
+    color: var(--green);
+  }
+`;
+
+const SkillList = styled.ul`
+  display: grid;
+  grid-template-columns: repeat(2, minmax(140px, 200px));
+  gap: 0px 10px;
+  padding: 0px;
+  margin: 20px 0px 0px;
+  overflow: hidden;
+  list-style: none;
+
+  margin-block-start: 1em;
+  margin-block-end: 1em;
+  margin-inline-start: 0px;
+  margin-inline-end: 0px;
+  padding-inline-start: 40px;
+  
+  li::before {
+  content: "> ";
+  font-size: 16px;
+  color: var(--green);
+}
+`;
+
+const ProfilePic = styled(Image)`
+  border-radius: 4px;
+`
+
+const Marked = styled.strong`
+  color: var(--green);
+`
+
+export const Ui = { Wrapper, Description, Title, SkillList, ProfilePic, Marked };
