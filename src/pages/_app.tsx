fa7831f4b4639718390694a658c7801192281442
emyslandim <emydl.moura@gmail.com>
2023-02-06 19:32:57 -0300

    Create global styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,20 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { Roboto_Mono } from '@next/font/google'
+import type { AppProps } from "next/app";
+import { Fragment } from 'react';
+import GlobalStyle from '@/styles/globalStyle';
+import Layout from '@/components/Layout';
+
+const RobotoMono = Roboto_Mono({ subsets: ['latin'] })
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={RobotoMono.className}>
+      <Fragment>
+      <GlobalStyle />
+      <Layout>
+      <Component {...pageProps} />
+      </Layout>
+    </Fragment>
+    </main>
+  );
 }
